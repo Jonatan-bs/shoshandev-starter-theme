40cd52a8b82667ecef2995fe243c083826a9d983
Jonatan-bs <jonatan-bs@live.dk>
2021-09-06 18:02:34 +0200

    update README

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@ if ( ! isset( $content_width ) ) $content_width = 900;
 add_theme_support( 'post-thumbnails' );
 register_nav_menus( 
 	array(
-		'primary'	=>	__( 'Header'), // Register the Primary menu
+		'primary'	=>	__( 'Header', 'shoshandevstartertheme'), // Register the Primary menu
 	)
 );
 
